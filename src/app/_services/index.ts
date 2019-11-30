49b96a487f9f0f3a465fb20e1c0ba475bdb4ae64
Jonatan Arrocha Kang <erickangweisz@gmail.com>
2019-11-30 22:07:13 +0000

    add userService and publicationService into index file

--- src/app/_services/index.ts
+++ src/app/_services/index.ts
@@ -1,1 +1,3 @@
 export { AuthenticationService } from './authentication/authentication.service';
+export { UserService } from './user/user.service';
+export { PublicationService } from './publication/publication.service';
