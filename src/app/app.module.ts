29e27aad55959ad29048a4e4bee916a902c97b14
Jonatan Arrocha Kang <erickangweisz@gmail.com>
2019-11-30 17:59:13 +0000

    generate navbar, login, landing and admin-dashboard component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    LandingComponent,
+    AdminDashboardComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
