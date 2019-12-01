280ab41b4fd1f58b9afade7c4d821a32ba73c06a
Jonatan Arrocha Kang <erickangweisz@gmail.com>
2019-12-01 23:07:31 +0000

    implement getByPublicationId function in answer-service

--- src/app/_services/answer/answer.service.ts
+++ src/app/_services/answer/answer.service.ts
@@ -1,9 +1,19 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { APP_CONFIG, AppConfig } from 'src/app/app-config.module';
+import { Answer } from 'src/app/_models';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AnswerService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+    @Inject(APP_CONFIG) private config: AppConfig
+  ) {}
+
+  getByPublicationId(publicationId: string) {
+    return this.http.get<Answer[]>(`${this.config.apiURL}/answers/${publicationId}`);
+  }
 }
