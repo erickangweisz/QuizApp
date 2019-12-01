dea799d873ef2a526e93b2b2f580e903d44b18ee
Jonatan Arrocha Kang <erickangweisz@gmail.com>
2019-12-01 13:33:47 +0000

    add routerlink directive in login-form to go register-form component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { LandingComponent,
+         RegisterFormComponent,
          AdminDashboardComponent } from './_components';
 
 import { AuthGuard } from './_guards';
 
 const routes: Routes = [
   { path: 'landing', component: LandingComponent },
+  { path: 'register', component: RegisterFormComponent},
   { path: 'admin-dashboard',
     component: AdminDashboardComponent,
     canActivate: [AuthGuard]
